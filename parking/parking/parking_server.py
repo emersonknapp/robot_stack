6cbc89c0cedb4cba36f46a7b88aa407576a3225b
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-12-27 23:22:54 -0800

    Serving a marker with 2 axes and rotation

--- parking/parking/parking_server.py
+++ parking/parking/parking_server.py
@@ -1,29 +1,56 @@
 #!/usr/bin/env python3
 
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Quaternion
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 import rclpy
 from rclpy.node import Node
 from robot_interfaces.srv import AddParkingSpot
 from robot_interfaces.srv import DeleteParkingSpot
 from visualization_msgs.msg import InteractiveMarker
-from geometry_msgs.msg import Pose
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import Quaternion
+from visualization_msgs.msg import InteractiveMarkerControl
+from visualization_msgs.msg import MenuEntry
 
 
 class ParkingSpotServer(Node):
     def __init__(self):
         super(ParkingSpotServer, self).__init__('parking_spot_server')
         self.spots = {}
-        # self.marker_server = InteractiveMarkerServer(self, 'parking')
-        # box_marker = InteractiveMarker(
-        #     name='box',
-        #     pose=Pose(
-        #         position=Point(x=2, y=1, z=0),
-        #         orientation=Quaternion(x=0, y=0, z=0, w=1),
-        #     )
-        # )
-        # self.marker_server.insert(box_marker)
+        self.marker_server = InteractiveMarkerServer(self, 'parking')
+        box_marker = InteractiveMarker(
+            name='box',
+            pose=Pose(
+                position=Point(x=2., y=1., z=0.),
+                orientation=Quaternion(x=0., y=0., z=0., w=1.),
+            )
+        )
+        box_marker.header.frame_id = 'map'
+        box_marker.menu_entries.append(
+            MenuEntry(id=1, parent_id=0, title='florp', command_type=0)
+        )
+        box_marker.controls = [
+            InteractiveMarkerControl(
+                name='xaxis',
+                orientation_mode=InteractiveMarkerControl.FIXED,
+                orientation=Quaternion(x=0., y=0., z=0.7068252, w=0.7068252),
+                interaction_mode=InteractiveMarkerControl.MOVE_AXIS,
+                # always_visible=True,
+            ),
+            InteractiveMarkerControl(
+                name='yaxis',
+                orientation_mode=InteractiveMarkerControl.FIXED,
+                interaction_mode=InteractiveMarkerControl.MOVE_AXIS,
+                # always_visible=True,
+            ),
+            InteractiveMarkerControl(
+                name='turn',
+                interaction_mode=InteractiveMarkerControl.ROTATE_AXIS,
+                orientation=Quaternion(x=0., y=0.7068252, z=0., w=0.7068252),
+            )
+        ]
+        self.marker_server.insert(box_marker)
+        self.marker_server.applyChanges()
 
         self.add_srv = self.create_service(
             AddParkingSpot, 'add_parking_spot', self.add_spot)
