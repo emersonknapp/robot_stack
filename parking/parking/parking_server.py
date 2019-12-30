1ad41c62eaef69d0eca8d0f592b1cfcbb9984505
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-12-30 01:35:00 -0800

    Display parkspot names on the interactive markers

--- parking/parking/parking_server.py
+++ parking/parking/parking_server.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Pose2D
 from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Vector3
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 import rclpy
 from rclpy.node import Node
@@ -12,9 +13,10 @@ from robot_interfaces.srv import AddParkingSpot
 from robot_interfaces.srv import DeleteParkingSpot
 from robot_interfaces.srv import GetParkingSpot
 from robot_interfaces.srv import RenameParkingSpot
+from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import InteractiveMarker
 from visualization_msgs.msg import InteractiveMarkerControl
-from visualization_msgs.msg import MenuEntry
+from visualization_msgs.msg import Marker
 
 
 # TODO(e) definitely have to make/find a linalg/utils library
@@ -40,6 +42,7 @@ class ParkingSpotServer(Node):
         super(ParkingSpotServer, self).__init__('parking_spot_server')
         self.spots = {}
         self.marker_server = InteractiveMarkerServer(self, 'parking')
+        # self.add_marker('test', Pose2D(x=1.1, y=0.5, theta=1.0))
 
         self.add_srv = self.create_service(
             AddParkingSpot, 'add_parking_spot', self.add_spot)
@@ -53,7 +56,7 @@ class ParkingSpotServer(Node):
     def box_feedback(self, fb) -> None:
         print(fb)
 
-    def create_marker(self, name: str, pose: Pose2D) -> InteractiveMarker:
+    def add_marker(self, name: str, pose: Pose2D) -> InteractiveMarker:
         marker = InteractiveMarker(
             name=name,
             pose=Pose(
@@ -62,7 +65,27 @@ class ParkingSpotServer(Node):
             )
         )
         marker.header.frame_id = 'map'
+
+        sc = 0.2
+        z = 0.1
+        name_marker = Marker(
+            type=Marker.TEXT_VIEW_FACING,
+            scale=Vector3(x=sc, y=sc, z=sc),
+            color=ColorRGBA(r=1., g=1., b=1., a=1.),
+            text=name,
+        )
+        name_marker.pose.position.x = sc * -0.1
+        name_marker.pose.position.z = z + sc * -0.1
+
         marker.controls = [
+            InteractiveMarkerControl(
+                name='name',
+                orientation_mode=InteractiveMarkerControl.VIEW_FACING,
+                interaction_mode=InteractiveMarkerControl.NONE,
+                independent_marker_orientation=False,
+                always_visible=True,
+                markers=[name_marker],
+            ),
             InteractiveMarkerControl(
                 name='xaxis',
                 orientation_mode=InteractiveMarkerControl.FIXED,
@@ -80,16 +103,16 @@ class ParkingSpotServer(Node):
                 orientation=Quaternion(x=0., y=0.7068252, z=0., w=0.7068252),
             )
         ]
+        self.marker_server.insert(marker, feedback_callback=self.box_feedback)
+        self.marker_server.applyChanges()
         return marker
 
     def add_spot(self, request, response):
         print(request)
-        name = 'park{:02}'.format(len(self.spots))
+        name = 'park{:02}'.format(len(self.spots) + 1)
         if name not in self.spots:
-            marker = self.create_marker(name, request.pose)
+            marker = self.add_marker(name, request.pose)
             # self.spots[name] = marker
-            self.marker_server.insert(marker, feedback_callback=self.box_feedback)
-            self.marker_server.applyChanges()
         self.spots[name] = request.pose
         response.success = True
         return response
