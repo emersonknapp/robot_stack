b996a632cfc9d80457ab67266331ff6742c3cc79
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-12-30 01:10:16 -0800

    Add parking srv adds a marker

--- parking/parking/parking_server.py
+++ parking/parking/parking_server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import math
 
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose2D
 from geometry_msgs.msg import Quaternion
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 import rclpy
@@ -15,23 +17,52 @@ from visualization_msgs.msg import InteractiveMarkerControl
 from visualization_msgs.msg import MenuEntry
 
 
+# TODO(e) definitely have to make/find a linalg/utils library
+def quaternion_from_euler(roll: float, pitch: float, yaw: float) -> Quaternion:
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    q = Quaternion()
+    q.x = cy * cp * cr + sy * sp * sr
+    q.y = cy * cp * sr - sy * sp * cr
+    q.z = sy * cp * sr + cy * sp * cr
+    q.w = sy * cp * cr - cy * sp * sr
+
+    return q
+
+
 class ParkingSpotServer(Node):
     def __init__(self):
         super(ParkingSpotServer, self).__init__('parking_spot_server')
         self.spots = {}
         self.marker_server = InteractiveMarkerServer(self, 'parking')
-        box_marker = InteractiveMarker(
-            name='box',
+
+        self.add_srv = self.create_service(
+            AddParkingSpot, 'add_parking_spot', self.add_spot)
+        self.del_srv = self.create_service(
+            DeleteParkingSpot, 'delete_parking_spot', self.delete_spot)
+        self.get_srv = self.create_service(
+            GetParkingSpot, 'get_parking_spot', self.get_spot)
+        self.rename_srv = self.create_service(
+            RenameParkingSpot, 'rename_parking_spot', self.rename_spot)
+
+    def box_feedback(self, fb) -> None:
+        print(fb)
+
+    def create_marker(self, name: str, pose: Pose2D) -> InteractiveMarker:
+        marker = InteractiveMarker(
+            name=name,
             pose=Pose(
-                position=Point(x=2., y=1., z=0.),
-                orientation=Quaternion(x=0., y=0., z=0., w=1.),
+                position=Point(x=pose.x, y=pose.y, z=0.),
+                orientation=quaternion_from_euler(pose.theta, 0., 0.),
             )
         )
-        box_marker.header.frame_id = 'map'
-        box_marker.menu_entries.append(
-            MenuEntry(id=1, parent_id=0, title='florp', command_type=0)
-        )
-        box_marker.controls = [
+        marker.header.frame_id = 'map'
+        marker.controls = [
             InteractiveMarkerControl(
                 name='xaxis',
                 orientation_mode=InteractiveMarkerControl.FIXED,
@@ -49,24 +80,16 @@ class ParkingSpotServer(Node):
                 orientation=Quaternion(x=0., y=0.7068252, z=0., w=0.7068252),
             )
         ]
-        self.marker_server.insert(box_marker, feedback_callback=self.box_feedback)
-        self.marker_server.applyChanges()
-
-        self.add_srv = self.create_service(
-            AddParkingSpot, 'add_parking_spot', self.add_spot)
-        self.del_srv = self.create_service(
-            DeleteParkingSpot, 'delete_parking_spot', self.delete_spot)
-        self.get_srv = self.create_service(
-            GetParkingSpot, 'get_parking_spot', self.get_spot)
-        self.rename_srv = self.create_service(
-            RenameParkingSpot, 'rename_parking_spot', self.rename_spot)
-
-    def box_feedback(self, fb):
-        print(fb)
+        return marker
 
     def add_spot(self, request, response):
         print(request)
         name = 'park{:02}'.format(len(self.spots))
+        if name not in self.spots:
+            marker = self.create_marker(name, request.pose)
+            # self.spots[name] = marker
+            self.marker_server.insert(marker, feedback_callback=self.box_feedback)
+            self.marker_server.applyChanges()
         self.spots[name] = request.pose
         response.success = True
         return response
