09ce824f8dc124c628954152e3df6d2515698b56
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-01-02 21:52:13 -0800

    Action server can do one nav goal. but not a next one. investigating

--- parking/parking/nav_server.py
+++ parking/parking/nav_server.py
@@ -30,12 +30,15 @@ from rclpy.node import Node
 from robot_interfaces.action import NavigateToParkingSpot
 from robot_interfaces.srv import GetParkingSpot
 
+import threading
+
 from .transformations import quaternion_from_euler
 
 
 class NavigateToParkingSpotActionServer(Node):
-    def __init__(self):
+    def __init__(self, executor):
         super().__init__('nav_to_parkspot')
+        self.executor = executor
         self._action_server = ActionServer(
             self,
             NavigateToParkingSpot,
@@ -48,22 +51,27 @@ class NavigateToParkingSpotActionServer(Node):
         self._action_client = ActionClient(
             self,
             NavigateToPose,
-            'navigate_to_pose',
+            'NavigateToPose',
         )
         self._get_parking_spot = self.create_client(GetParkingSpot, 'get_parking_spot')
+        self.has_nav_goal_event = threading.Event()
 
     def destroy(self):
         self._action_server.destroy()
         super().destroy_node()
 
     def receive_park_goal(self, goal_request):
-        self.get_logger().info('Received park goal request')
+        self.get_logger().info('Received park goal request {} {}'.format(
+            type(goal_request), goal_request))
         nav_goal_msg = NavigateToPose.Goal()
         spot_name = goal_request.name
+        self.get_logger().info('Finding parking spot {}'.format(spot_name))
         response = self._get_parking_spot.call(
             GetParkingSpot.Request(name=spot_name))
         if not response.success:
+            self.get_logger().info('Couldnt get that parking spot')
             return GoalResponse.REJECT
+        self.get_logger().info('Found parking spot')
 
         spot2d = response.pose
         pose_msg = PoseStamped()
@@ -73,13 +81,13 @@ class NavigateToParkingSpotActionServer(Node):
         q = quaternion_from_euler(spot2d.theta, 0., 0.)
         x, y, z, w = q
         pose_msg.pose.orientation = Quaternion(x=x, y=y, z=z, w=w)
+        self.get_logger().info('Finished constructing pose {}'.format(pose_msg))
         nav_goal_msg.pose = pose_msg
 
         self._action_client.wait_for_server()
         self._send_goal_future = self._action_client.send_goal_async(
             nav_goal_msg,
             feedback_callback=self.nav_goal_feedback)
-
         self._send_goal_future.add_done_callback(self.nav_goal_response)
         return GoalResponse.ACCEPT
 
@@ -99,7 +107,10 @@ class NavigateToParkingSpotActionServer(Node):
     async def execute_park_goal(self, park_goal_handle):
         self.get_logger().info('Executing park goal...')
 
-        rclpy.spin_until_future_complete(self._get_result_future)
+        rclpy.spin_until_future_complete(self, self._send_goal_future)
+        self.has_nav_goal_event.wait()
+        rclpy.spin_until_future_complete(self, self._get_result_future)
+
         status = self._get_result_future.result().status
         if status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().info('Nav goal succeeded, succeeding park goal!')
@@ -107,8 +118,8 @@ class NavigateToParkingSpotActionServer(Node):
         else:
             self.get_logger().info('Nav goal failed with status: {0}'.format(status))
             park_goal_handle.fail()
+
         result = NavigateToParkingSpot.Result()
-        self.get_logger.info('Returning park result')
         return result
 
     def nav_goal_response(self, future):
@@ -119,26 +130,25 @@ class NavigateToParkingSpotActionServer(Node):
             return
 
         self.get_logger().info('Nav Goal accepted :)')
-        self._nav_goal_handle = nav_goal_handle
         self._get_result_future = nav_goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.nav_goal_result)
-
-    def nav_goal_result(self, future):
-        result = future.result().result
-        self.get_logger().info('Nav Goal Result: {0}'.format(result.sequence))
+        self.has_nav_goal_event.set()
 
     def nav_goal_feedback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        feedback_msg = NavigateToParkingSpot.Feedback()
-        self._nav_goal_handle.publish_feedback(feedback_msg)
-        self.get_logger().info('Received nav goal feedback: {0}'.format(feedback.partial_sequence))
+        self.get_logger().info('Received nav goal feedback: {}'.format(feedback))
+
+    def nav_goal_result(self, future):
+        result = future.result().result
+        self.get_logger().info('Nav Goal Result: {}'.format(result))
 
 
 def main():
     rclpy.init()
-    node = NavigateToParkingSpotActionServer()
     executor = MultiThreadedExecutor()
-    rclpy.spin(node, executor=executor)
+    node = NavigateToParkingSpotActionServer(executor)
+    executor.add_node(node)
+    executor.spin()
     node.destroy()
     rclpy.shutdown()
 
