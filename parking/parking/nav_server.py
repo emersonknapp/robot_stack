9defcaf259112ae82d7cb480133c557a844f71e0
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-01-02 23:52:17 -0800

    wip - simplified code

--- parking/parking/nav_server.py
+++ parking/parking/nav_server.py
@@ -30,8 +30,6 @@ from rclpy.node import Node
 from robot_interfaces.action import NavigateToParkingSpot
 from robot_interfaces.srv import GetParkingSpot
 
-import threading
-
 from .transformations import quaternion_from_euler
 
 
@@ -46,33 +44,47 @@ class NavigateToParkingSpotActionServer(Node):
             execute_callback=self.execute_park_goal,
             callback_group=ReentrantCallbackGroup(),
             goal_callback=self.receive_park_goal,
-            cancel_callback=self.cancel_park_goal,
+            # cancel_callback=self.cancel_park_goal,
         )
         self._action_client = ActionClient(
             self,
             NavigateToPose,
             'NavigateToPose',
+            callback_group=ReentrantCallbackGroup(),
         )
         self._get_parking_spot = self.create_client(GetParkingSpot, 'get_parking_spot')
-        self.has_nav_goal_event = threading.Event()
 
-    def destroy(self):
-        self._action_server.destroy()
-        super().destroy_node()
+    # def destroy(self):
+    #     self._action_server.destroy()
+    #     super().destroy_node()
 
     def receive_park_goal(self, goal_request):
-        self.get_logger().info('Received park goal request {} {}'.format(
-            type(goal_request), goal_request))
-        nav_goal_msg = NavigateToPose.Goal()
-        spot_name = goal_request.name
-        self.get_logger().info('Finding parking spot {}'.format(spot_name))
+        self.get_logger().info('RECEIVE PARK GOALLLL')
+        return GoalResponse.ACCEPT
+
+    # def cancel_park_goal(self, park_goal_handle):
+    #     self.get_logger().info('Received park goal cancel request')
+    #     future = self._nav_goal_handle.cancel_goal_async()
+    #     future.add_done_callback(self.cancel_done)
+    #     return CancelResponse.ACCEPT
+
+    async def execute_park_goal(self, park_goal_handle):
+        self.get_logger().info('Executing park goal...')
+        request = park_goal_handle.request
+        # rclpy.spin_until_future_complete(self, self._send_goal_future)
+        # rclpy.spin_until_future_complete(self, self._get_result_future)
+        self.get_logger().info('Received park goal request {} {}'.format(type(request), request))
+        self.get_logger().info('Finding parking spot {}'.format(request.name))
+
+        # Retrieve parking spot Pose 2D
         response = self._get_parking_spot.call(
-            GetParkingSpot.Request(name=spot_name))
+            GetParkingSpot.Request(name=request.name))
         if not response.success:
             self.get_logger().info('Couldnt get that parking spot')
             return GoalResponse.REJECT
         self.get_logger().info('Found parking spot')
 
+        # Convert Pose2D into 3D pose for nav
         spot2d = response.pose
         pose_msg = PoseStamped()
         pose_msg.header.stamp = self.get_clock().now().to_msg()
@@ -81,37 +93,27 @@ class NavigateToParkingSpotActionServer(Node):
         q = quaternion_from_euler(spot2d.theta, 0., 0.)
         x, y, z, w = q
         pose_msg.pose.orientation = Quaternion(x=x, y=y, z=z, w=w)
-        self.get_logger().info('Finished constructing pose {}'.format(pose_msg))
+
+        nav_goal_msg = NavigateToPose.Goal()
         nav_goal_msg.pose = pose_msg
 
+        # Do the action portion
         self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(
-            nav_goal_msg,
-            feedback_callback=self.nav_goal_feedback)
-        self._send_goal_future.add_done_callback(self.nav_goal_response)
-        return GoalResponse.ACCEPT
+        send_goal_future = self._action_client.send_goal_async(nav_goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+        goal_handle = send_goal_future.result()
 
-    def cancel_done(self, future):
-        cancel_response = future.result()
-        if len(cancel_response.goals_canceling) > 0:
-            self.get_logger().info('Goal successfully canceled')
-        else:
-            self.get_logger().info('Goal failed to cancel')
+        if not goal_handle.accepted:
+            self.get_logger().info('Nav goal rejected :(')
+            result = NavigateToParkingSpot.Result()
+            return result
 
-    def cancel_park_goal(self, park_goal_handle):
-        self.get_logger().info('Received park goal cancel request')
-        future = self._nav_goal_handle.cancel_goal_async()
-        future.add_done_callback(self.cancel_done)
-        return CancelResponse.ACCEPT
+        self.get_logger().info('Nav Goal accepted :)')
 
-    async def execute_park_goal(self, park_goal_handle):
-        self.get_logger().info('Executing park goal...')
+        get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, get_result_future)
 
-        rclpy.spin_until_future_complete(self, self._send_goal_future)
-        self.has_nav_goal_event.wait()
-        rclpy.spin_until_future_complete(self, self._get_result_future)
-
-        status = self._get_result_future.result().status
+        status = get_result_future.result().status
         if status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().info('Nav goal succeeded, succeeding park goal!')
             park_goal_handle.succeed()
@@ -122,25 +124,19 @@ class NavigateToParkingSpotActionServer(Node):
         result = NavigateToParkingSpot.Result()
         return result
 
-    def nav_goal_response(self, future):
-        nav_goal_handle = future.result()
+    # def nav_goal_response(self, future):
+    #     nav_goal_handle = future.result()
+    #
+    #     if not nav_goal_handle.accepted:
+    #         self.get_logger().info('Nav goal rejected :(')
+    #         return
+    #
+    #     self.get_logger().info('Nav Goal accepted :)')
+    #     self._get_result_future = nav_goal_handle.get_result_async()
 
-        if not nav_goal_handle.accepted:
-            self.get_logger().info('Nav goal rejected :(')
-            return
-
-        self.get_logger().info('Nav Goal accepted :)')
-        self._get_result_future = nav_goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.nav_goal_result)
-        self.has_nav_goal_event.set()
-
-    def nav_goal_feedback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-        self.get_logger().info('Received nav goal feedback: {}'.format(feedback))
-
-    def nav_goal_result(self, future):
-        result = future.result().result
-        self.get_logger().info('Nav Goal Result: {}'.format(result))
+    # def nav_goal_feedback(self, feedback_msg):
+    #     feedback = feedback_msg.feedback
+    #     self.get_logger().info('Received nav goal feedback: {}'.format(feedback))
 
 
 def main():
@@ -149,7 +145,7 @@ def main():
     node = NavigateToParkingSpotActionServer(executor)
     executor.add_node(node)
     executor.spin()
-    node.destroy()
+    # node.destroy()
     rclpy.shutdown()
 
 
