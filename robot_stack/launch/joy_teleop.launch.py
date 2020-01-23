32508c8b86e75b41878cee7c0ad07ace8816b969
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-01-23 10:03:59 -0800

    WIP separate headless stack to avoid big deps

---
+++ robot_stack/launch/joy_teleop.launch.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Copyright 2020 Emerson Knapp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    stack_share = get_package_share_directory('robot_stack')
+    teleop_params_file = os.path.join(stack_share, 'config', 'teleop_xbox.config.yaml')
+
+    standard_params = {'use_sim_time': LaunchConfiguration('use_sim_time')}
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+
+        # Teleop
+        Node(
+            package='joy',
+            node_executable='joy_node',
+            node_name='joy_driver',
+            parameters=[standard_params],
+            output='screen',
+        ),
+        Node(
+            package='teleop_twist_joy',
+            node_executable='teleop_node',
+            node_name='joy_interpreter',
+            parameters=[
+                teleop_params_file,
+                standard_params,
+            ],
+            output='screen',
+        ),
+    ])
